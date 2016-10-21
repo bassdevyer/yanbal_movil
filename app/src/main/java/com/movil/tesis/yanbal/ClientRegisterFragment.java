6f76b33724033537582db5b1ed547c48c4d808ce
miguetv17 <miguetv17@gmail.com>
2016-10-20 22:19:50 -0500

    Adds map

--- app/src/main/java/com/movil/tesis/yanbal/ClientRegisterFragment.java
+++ app/src/main/java/com/movil/tesis/yanbal/ClientRegisterFragment.java
@@ -1,10 +1,12 @@
 package com.movil.tesis.yanbal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by mac on 10/18/16.
@@ -24,6 +26,20 @@ public class ClientRegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_client_register, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_client_register, container, false);
+        inflateViews(rootView);
+        return rootView;
+    }
+
+    private void inflateViews(View rootView) {
+        ImageButton locationImageButton = (ImageButton) rootView.findViewById(R.id.clientLocationButton);
+        locationImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapActivityIntent = new Intent(getActivity(), MapActivity.class);
+                getActivity().startActivity(mapActivityIntent);
+            }
+        });
+
     }
 }
